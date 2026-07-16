76c52379232e8bc8ec8f14ad00a67ae3d520f12f
Arouna-kz <zkone403@gmail.com>
2026-07-17 00:16:26 +0200

    chore: bump AGP/Gradle (Android Studio)

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -3,8 +3,8 @@
  * `apply false` : les versions sont fixées ici, chaque module applique le plugin sans version.
  */
 plugins {
-    id("com.android.application") version "8.6.0" apply false
-    id("com.android.library") version "8.6.0" apply false
+    id("com.android.application") version "8.13.2" apply false
+    id("com.android.library") version "8.13.2" apply false
     id("org.jetbrains.kotlin.android") version "2.0.20" apply false
     id("org.jetbrains.kotlin.multiplatform") version "2.0.20" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.0.20" apply false
